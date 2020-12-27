7542ab351f399199b2edcbd1486494933d86f2a7
Michael Zappa <zapprich@gmail.com>
2020-12-27 13:19:26 -0600

    git ignore to ignore downloaded packages
    Modified-by: Michael Zappa <zapprich@gmail.com>

--- init.el
+++ init.el
@@ -1,15 +1,22 @@
-;; Heavily "inspired" by hlissner's DOOM distribution
+;; TODO
+;; magit
+;; lang and flycheck
+;; show file names when searching
+;; magit
+;; use purcell/emacs.d and DOOM as inspiration/sources
+;; finally, organize and put in ORG file
 
 (setq user-emacs-directory "~/my-emacs.d")
+
+(require 'package)
+(add-to-list `package-archives `("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
 ;; (setq inhibit-startup-message t)
 (setq ring-bell-function 'ignore)
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-
-(require 'package)
-(add-to-list `package-archives `("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
 
 (require 'evil)
 ;;(evil-mode 1)
@@ -26,18 +33,5 @@
 (require 'which-key)
 (which-key-mode)
 
-;; Custom
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'magit)
+
