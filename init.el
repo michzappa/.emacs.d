c5686f9c120abd173ed845e3b021fa90f1863c7d
Michael Zappa <zapprich@gmail.com>
2021-01-06 19:09:31 -0600

    going through hrs's config, committing before too much changes.

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(provide 'init)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -6,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cargo rust-mode elixir-mode lsp-ui lsp-mode company flycheck which-key treemacs-projectile treemacs-magit treemacs smex rainbow-delimiters projectile nov magit exec-path-from-shell counsel nord-theme diminish use-package))))
+    (lsp-treemacs lsp-ivy auto-compile cargo rust-mode elixir-mode lsp-ui lsp-mode company flycheck which-key treemacs-projectile treemacs-magit treemacs smex rainbow-delimiters projectile nov magit exec-path-from-shell counsel nord-theme diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
