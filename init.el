f4ae84142bcb47a36cff9f1bf672517230455a6b
Michael Zappa <zapprich@gmail.com>
2021-01-08 11:24:32 -0600

    custom-file.

--- init.el
+++ init.el
@@ -1,23 +1,4 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(load-file "~/.emacs.d/custom.el")
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(counsel-mode t)
- '(package-selected-packages
-	 '(free-keys doom-themes zenburn-theme mix dune tuareg python-mode lsp-python-ms paredit org-bullets company-restclient restclient lsp-treemacs lsp-ivy auto-compile cargo rust-mode elixir-mode lsp-ui lsp-mode company flycheck which-key treemacs-projectile treemacs-magit treemacs smex rainbow-delimiters projectile nov magit exec-path-from-shell counsel nord-theme diminish use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
