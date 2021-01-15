a4201bcc0b4a47a63b6caff5e5280366ba35acb6
Michael Zappa <zapprich@gmail.com>
2021-01-14 20:36:47 -0500

    exwm enhancements for bluetooth, monitor, and battery display.

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -3,5 +3,8 @@
 # Source .profile for common environment vars
 . ~/.profile
 
+# Enable screen locking on suspend
+xss-lock --slock &
+
 # Fire it up
 exec dbus-launch --exit-with-session emacs -mm --debug-init --use-exwm
