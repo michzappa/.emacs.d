f95ede33e8ce046b6a91442c6435759969300677
Michael Zappa <zapprich@gmail.com>
2021-01-19 15:07:00 -0500

    add custom.el file to git

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 '(desktop-environment bluetooth which-key web-mode vterm use-package tuareg treemacs-projectile treemacs-magit tide smex rainbow-delimiters pdf-tools paredit org-projectile org-present org-bullets nov mvn modus-vivendi-theme mix minions lsp-ui lsp-java lsp-ivy ivy-hydra ibuffer-projectile format-all exwm exec-path-from-shell elixir-mode elfeed-org dune counsel company-auctex cargo auto-compile all-the-icons ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
