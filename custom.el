d90d810651d8f46974da7b512d09110895d3a242
Michael Zappa <zapprich@gmail.com>
2021-01-21 19:50:47 -0500

    continuous scroll in pdf-mode, with quelpa.

--- custom.el
+++ custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ivy-mode t)
  '(package-selected-packages
-	 '(desktop-environment bluetooth which-key web-mode vterm use-package tuareg treemacs-projectile treemacs-magit tide smex rainbow-delimiters pdf-tools paredit org-projectile org-present org-bullets nov mvn modus-vivendi-theme mix minions lsp-ui lsp-java lsp-ivy ivy-hydra ibuffer-projectile format-all exwm exec-path-from-shell elixir-mode elfeed-org dune counsel company-auctex cargo auto-compile all-the-icons ag)))
+	 '(pdf-continuous-scroll-mode quelpa desktop-environment bluetooth which-key web-mode vterm use-package tuareg treemacs-projectile treemacs-magit tide smex rainbow-delimiters pdf-tools paredit org-projectile org-present org-bullets nov mvn modus-vivendi-theme mix minions lsp-ui lsp-java lsp-ivy ivy-hydra ibuffer-projectile format-all exwm exec-path-from-shell elixir-mode elfeed-org dune counsel company-auctex cargo auto-compile all-the-icons ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
