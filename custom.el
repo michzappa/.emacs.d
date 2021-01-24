3053d101cef5196c6ce307c5b3621bde6231179c
Michael Zappa <zapprich@gmail.com>
2021-01-23 22:08:38 -0500

    added eglot, going to phase out treemacs, flycheck, and lsp-mode.

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(org-agenda-files
 	 '("~/Org/ling3422.org" "~/Org/cy2550.org" "~/Org/cs3700.org" "~/Org/cs3650.org" "~/Co-Op/coop.org"))
  '(package-selected-packages
-	 '(pdf-continuous-scroll-mode quelpa desktop-environment bluetooth which-key web-mode vterm use-package tuareg treemacs-projectile treemacs-magit tide smex rainbow-delimiters pdf-tools paredit org-projectile org-present org-bullets nov mvn modus-vivendi-theme mix minions lsp-ui lsp-java lsp-ivy ivy-hydra ibuffer-projectile format-all exwm exec-path-from-shell elixir-mode elfeed-org dune counsel company-auctex cargo auto-compile all-the-icons ag)))
+	 '(eglot pdf-continuous-scroll-mode quelpa desktop-environment bluetooth which-key web-mode vterm use-package tuareg treemacs-projectile treemacs-magit tide smex rainbow-delimiters pdf-tools paredit org-projectile org-present org-bullets nov mvn modus-vivendi-theme mix minions lsp-ui lsp-java lsp-ivy ivy-hydra ibuffer-projectile format-all exwm exec-path-from-shell elixir-mode elfeed-org dune counsel company-auctex cargo auto-compile all-the-icons ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
