392dab2bbe145e6db162aaf85d28aaee5d971990
Michael Zappa <michzappa31@gmail.com>
2021-06-14 17:40:27 -0400

    proper loading of org file

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(package-initialize)
-(org-babel-load-file (expand-file-name "README.org"))
+(org-babel-load-file (expand-file-name "README.org" "~/.emacs.d"))
 (when (file-exists-p custom-file)
   (load-file custom-file))
